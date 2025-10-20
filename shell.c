4cfa2c311dcfbef96bd0b8903ce0d2d2886a4ea0
hfujita <c07014855@gmail.com>
2025-10-20 15:47:57 +0900

    pycall trial

--- shell.c
+++ shell.c
@@ -28801,6 +28801,11 @@ static int faultsim_callback(int iArg){
 **
 ** Return 1 on error, 2 to exit, and 0 otherwise.
 */
+
+/* added */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
 static int do_meta_command(char *zLine, ShellState *p){
   int h = 1;
   int nArg = 0;
@@ -30164,6 +30169,70 @@ static int do_meta_command(char *zLine, ShellState *p){
       rc = 1;
     }
   }else
+/* added */
+  if( c=='l' && cli_strncmp(azArg[0], "load_py", n)==0 )
+  {
+	PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    if (nArg < 3) {
+        fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+    }
+
+    Py_Initialize();
+	PyRun_SimpleString("import sys, os; sys.path.insert(0, os.getcwd())");
+    pName = PyUnicode_DecodeFSDefault(azArg[1]);
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, azArg[2]);
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(nArg - 3);
+            for (i = 0; i < nArg - 3; ++i) {
+                pValue = PyLong_FromLong(atoi(azArg[i + 3]));
+                if (!pValue) {
+                    Py_DECREF(pArgs);
+                    Py_DECREF(pModule);
+                    fprintf(stderr, "Cannot convert argument\n");
+                }
+                /* pValue reference stolen here: */
+                PyTuple_SetItem(pArgs, i, pValue);
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != NULL) {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+            }
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", azArg[2]);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", azArg[1]);
+    }
+    if (Py_FinalizeEx() < 0) {
+    }
+  }
+  else
 #endif
 
   if( c=='l' && cli_strncmp(azArg[0], "log", n)==0 ){
